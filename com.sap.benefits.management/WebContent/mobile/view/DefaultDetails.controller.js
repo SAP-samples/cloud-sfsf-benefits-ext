3387bd7774c0742330eae7c4093820e5bc636a8d
Marin Hadzhiev <marin.hadzhiev@sap.com>
2013-10-18 15:29:33 +0300

    initial clean up
    Change-Id: I74b1b9428891fb138670bbb343507391a551e295

--- com.sap.benefits.management/WebContent/mobile/view/DefaultDetails.controller.js
+++
@@ -1,7 +0,0 @@
-sap.ui.controller("com.sap.hana.cloud.samples.benefits.view.DefaultDetails", {
-    onInit: function() {
-
-    },
-    onAfterRendering: function() {
-    },
-});
--- com.sap.benefits.management/src/com/sap/hana/cloud/samples/benefits/persistence/manager/EntityManagerProvider.java
+++
@@ -1,9 +0,0 @@
-package com.sap.hana.cloud.samples.benefits.persistence.manager;
-
-import javax.persistence.EntityManager;
-
-public interface EntityManagerProvider {
-
-	EntityManager get();
-
-}
