678a2674954c6119f9a047509cd5a5cea400beaa
Marin Hadzhiev <marin.hadzhiev@sap.com>
2013-11-18 17:16:57 +0300

    fix some findBugs issues
    Change-Id: I9f45ae66365bb55e7a6f7946b938fac4002245a8

--- com.sap.hana.cloud.samples.benefits/src/com/sap/hana/cloud/samples/benefits/api/BaseService.java
+++ com.sap.hana.cloud.samples.benefits/src/com/sap/hana/cloud/samples/benefits/api/BaseService.java
@@ -14,45 +14,43 @@ import com.sap.hana.cloud.samples.benefits.persistence.model.User;
 
 public class BaseService {
 
-    protected static final String ADMIN_ROLE = "Administrator";
+	protected static final String ADMIN_ROLE = "Administrator";
 
-    @Resource
-    protected HttpServletRequest request;
+	@Resource
+	protected HttpServletRequest request;
 
-    @Resource
-    protected HttpServletResponse response;
+	@Resource
+	protected HttpServletResponse response;
 
-    @Resource
-    protected ServletContext context;
+	@Resource
+	protected ServletContext context;
 
-    protected UserDAO userDAO = new UserDAO();
+	protected UserDAO userDAO = new UserDAO();
 
-    protected String getLoggedInUserId() {
-        if (request == null) {
-            throw new IllegalArgumentException("Request must not be null.");
-        }
-        String userId = null;
+	protected String getLoggedInUserId() {
+		if (request == null) {
+			throw new IllegalArgumentException("Request must not be null.");
+		}
+		String userId = null;
 
-        if (request instanceof HttpServletRequest) {
-            Principal userPrincipal = ((HttpServletRequest) request).getUserPrincipal();
-            if (userPrincipal != null) {
-                userId = userPrincipal.getName();
-            }
-        }
+		Principal userPrincipal = ((HttpServletRequest) request).getUserPrincipal();
+		if (userPrincipal != null) {
+			userId = userPrincipal.getName();
+		}
 
-        return userId;
-    }
+		return userId;
+	}
 
-    protected User getLoggedInUser() {
-        return userDAO.getByUserId(getLoggedInUserId());
-    }
+	protected User getLoggedInUser() {
+		return userDAO.getByUserId(getLoggedInUserId());
+	}
 
-    protected Response createBadRequestResponse(String reponseText) {
-        return Response.status(Status.BAD_REQUEST).entity(reponseText).build();
-    }
+	protected Response createBadRequestResponse(String reponseText) {
+		return Response.status(Status.BAD_REQUEST).entity(reponseText).build();
+	}
 
-    protected Response createOkResponse() {
-        return Response.ok().build();
-    }
+	protected Response createOkResponse() {
+		return Response.ok().build();
+	}
 
 }
